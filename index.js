6e450a09dc726b1b4488c47579345ae038dc64a4
NothileC <nothilecmhlanga@outlook.com>
2024-03-24 04:32:18 +0800

    updated html/css/js

--- index.js
+++ index.js
@@ -1,144 +1,27 @@
-input_credit_card = function(input)
-{
-    var format_and_pos = function(char, backspace)
-    {
-        var start = 0;
-        var end = 0;
-        var pos = 0;
-        var separator = " ";
-        var value = input.value;
 
-        if (char !== false)
-        {
-            start = input.selectionStart;
-            end = input.selectionEnd;
+let validNumber = false;
+let validCvv = false; 
+let validMonth = false;
+let validYear = false;
+//Adding event listener for payment button using a styled class
+ const openPopup = document.getElementById("btn-pay");
+ const closePopup = document.getElementById("btn-ok");
+ const popup = document.getElementById("popup-id");
 
-            if (backspace && start > 0) // handle backspace onkeydown
-            {
-                start--;
+//opening popup
+openPopup.addEventListener("click", () => {
+    popup.classList.add("open-popup");
 
-                if (value[start] == separator)
-                { start--; }
-            }
-            // To be able to replace the selection if there is one
-            value = value.substring(0, start) + char + value.substring(end);
-
-            pos = start + char.length; // caret position
-        }
-
-        var d = 0; // digit count
-        var dd = 0; // total
-        var gi = 0; // group index
-        var newV = "";
-        var groups = /^\D*3[47]/.test(value) ? // check for American Express
-        [4, 6, 5] : [4, 4, 4, 4];
-
-        for (var i = 0; i < value.length; i++)
-        {
-            if (/\D/.test(value[i]))
-            {
-                if (start > i)
-                { pos--; }
-            }
-            else
-            {
-                if (d === groups[gi])
-                {
-                    newV += separator;
-                    d = 0;
-                    gi++;
-
-                    if (start >= i)
-                    { pos++; }
-                }
-                newV += value[i];
-                d++;
-                dd++;
-            }
-            if (d === groups[gi] && groups.length === gi + 1) // max length
-            { break; }
-        }
-        input.value = newV;
-
-        if (char !== false)
-        { input.setSelectionRange(pos, pos); }
-    };
-
-    input.addEventListener('keypress', function(e)
-    {
-        var code = e.charCode || e.keyCode || e.which;
-
-        // Check for tab and arrow keys (needed in Firefox)
-        if (code !== 9 && (code < 37 || code > 40) &&
-        // and CTRL+C / CTRL+V
-        !(e.ctrlKey && (code === 99 || code === 118)))
-        {
-            e.preventDefault();
-
-            var char = String.fromCharCode(code);
-
-            // if the character is non-digit
-            // OR
-            // if the value already contains 15/16 digits and there is no selection
-            // -> return false (the character is not inserted)
-
-            if (/\D/.test(char) || (this.selectionStart === this.selectionEnd &&
-            this.value.replace(/\D/g, '').length >=
-            (/^\D*3[47]/.test(this.value) ? 15 : 16))) // 15 digits if Amex
-            {
-                return false;
-            }
-            format_and_pos(char);
-        }
-    });
-    
-    // backspace doesn't fire the keypress event
-    input.addEventListener('keydown', function(e)
-    {
-        if (e.keyCode === 8 || e.keyCode === 46) // backspace or delete
-        {
-            e.preventDefault();
-            format_and_pos('', this.selectionStart === this.selectionEnd);
-        }
-    });
-    
-    input.addEventListener('paste', function()
-    {
-        // A timeout is needed to get the new value pasted
-        setTimeout(function(){ format_and_pos(''); }, 50);
-    });
-    
-    input.addEventListener('blur', function()
-    {
-    	// reformat onblur just in case (optional)
-        format_and_pos(this, false);
-    });
-    //append card number to virtual card (show number to card)...................................
-    input.addEventListener('keyup', function() {
-         document.getElementById('card-number').innerHTML = this.value;
-    })
-};
-
-input_credit_card(document.getElementById('credit-card'));
-
-//append card name holder input to virtual card.............................................
-document.getElementById('card-name').addEventListener('keyup', function() {
-    document.querySelector('.name-holder').innerHTML = this.value;
+    clearForm();
 });
 
-//append expiration month input to virtual card............................................
-document.getElementById('exp-month').addEventListener('keyup', function() {
-    document.querySelector('.exp-month').innerHTML = this.value;
-});
-
-//append expiration year input to virtual card.........................................
-document.getElementById('exp-year').addEventListener('keyup', function() {
-    document.querySelector('.exp-year').innerHTML = this.value;
+//closing popup
+closePopup.addEventListener("click", () => {
+    popup.classList.remove("open-popup");
 });
 
 
-
-//setting up the function for time left!..........................................
+//setting up the function for time left!
 function startTimer(duration, display) {
     let timer = duration, minutes, seconds;
     setInterval(function () {
@@ -163,38 +46,141 @@ window.onload = function () {
 };
 
 
-//Event listener to open and close popup...............
-const openPopup = document.getElementById("openPopup");
-const closePopup = document.getElementById("closePopup");
-const popup = document.getElementById("popup");
-
-//click event to add or remove popup
-openPopup.addEventListener("click", () => {
-    popup.classList.add("open-popup");
-});
-
-closePopup.addEventListener("click", () => {
-    popup.classList.remove("open-popup");
-});
-
-function toggle (){
-    let page = document.getElementById("page");
-    page.classList
+//validating the cvv on the card
+function validateCvv(){
+    let cvv = document.getElementById("cvv").value;
+    //if statement
+    if(parseInt(cvv) < 3){
+        document.getElementById("cvv-error").innerHTML = "Invalid CVV";
+        validCvv = false;
+        document.getElementById("cvv-error").style.color = "red";
+    
+    }
+    else{
+        document.getElementById("cvv-error").innerHTML = "";
+        validCvv = true;
+    }
 }
 
-//function to enable form submission only when all fields have been filled.........
-//function checkform(){
-    //let f = document.forms['theform'].elements;
-    //let fieldsMustBeFilled = true;
 
-    //for (var i = 0; i < f.length; i++){
-        //if(f[i].value.length == 0)
-        //fieldsMustBeFilled = false;
-    //}
-    //if(fieldsMustBeFilled){
-        //document.getElementById("popup").disabled = false;
-   // }
-    //else{
-       // document.getElementById("popup").disabled = true;
-   // }
-//}
+//Validating the month on card
+function validateMonth(){
+    let month = document.getElementById("exp-month").value;
+    
+     // Display a message based on the validity of the card month    
+    if (parseInt(month) < 1 || parseInt(month) > 12){
+        document.getElementById("month-error").innerHTML = "Invalid Month"; 
+        validMonth = false;
+        document.getElementById("month-error").style.color= "red";
+    }
+    else{
+        document.getElementById("month-error").innerHTML = "";
+        validMonth = true;
+    }
+}
+
+
+//validating the year on card
+function validateYear(){
+    let year = document.getElementById("exp-year").value;
+
+     // Display a message based on the validity of the card year    
+    if (parseInt(year) < new Date().getFullYear()){
+        document.getElementById("year-error").innerHTML = "Invalid Year";
+        validYear = false;
+        document.getElementById("year-error").style.color = "red";
+    }
+    else{
+        document.getElementById("year-error").innerHTML = "";
+        validYear = true;
+    }
+}
+
+//function validating the card number
+function validateCard() { 
+    // Retrieve the card number from the input field             
+    let cardNumber = document.getElementById('credit-card').value; 
+
+    // Remove spaces and dashes for easier processing            
+    cardNumber = cardNumber.replace(/\s|-/g, ''); 
+
+    // Check if the card number is valid using Luhn's Algorithm             
+    let isValid = luhnCheck(cardNumber);  
+
+    // Display a message based on the validity of the card number             
+    if(isValid == false) {
+        document.getElementById("card-error").innerHTML = "Invalid Card Number"; 
+        validNumber = false; 
+        document.getElementById("card-error").style.color = "red";
+    } 
+    else{          
+        document.getElementById("card-error").innerHTML = ""; 
+        validNumber = true;
+    }       
+ }         
+
+function luhnCheck(cardNumber) { 
+    var sum = 0;             
+    var shouldDouble = false;             
+    // Iterate through the card number from right to left             
+    for (var i = cardNumber.length - 1; i >= 0; i--) {                
+            var digit = parseInt(cardNumber.charAt(i));                 
+            if (shouldDouble) {                     digit *= 2; 
+            // Double every second digit                     
+            if (digit > 9) {                         digit -= 9; 
+                // Subtract 9 if the doubled digit is greater than 9                    
+                }                
+        }                 
+                sum += digit;                 
+                shouldDouble = !shouldDouble; 
+                // Alternate the flag             
+    }             
+    // A valid card number will have a total sum that is a multiple of 10             
+    return (sum % 10) === 0;         
+}
+
+
+ 
+//function to activate the payment button
+function authorisePayment(){
+    const cardNumber = document.getElementById("credit-card").value.trim();
+        const cardName = document.getElementById("card-name").value.trim();
+        const cardCvv = document.getElementById("cvv").value.trim();
+        const cardMonth = document.getElementById("exp-month").value.trim();
+        const cardYear = document.getElementById("exp-year").value.trim();
+
+      document.getElementById("btn-pay").disabled = !(cardNumber&& cardName&& cardCvv&& cardMonth&& cardYear&& validNumber&& validCvv&& validMonth&& validYear);
+
+}
+
+//function to clear the form after payment
+function clearForm(){
+    let element = document.getElementById("registration-form");          
+    element.reset();
+
+
+ //disable pay button after payment
+    document.getElementById("btn-pay").disabled = true;
+    
+}
+
+ //Event listener to show card inforrmation on the virtual card
+ //append card number to virtual card......................................................
+ document.getElementById('credit-card').addEventListener('keyup', function(){
+    document.querySelector('.last-digit').innerHTML = this.value;
+ }); 
+//append card name holder input to virtual card.............................................
+document.getElementById('card-name').addEventListener('keyup', function() {
+    document.querySelector('.name-holder').innerHTML = this.value;
+});
+
+//append expiration month input to virtual card............................................
+document.getElementById('exp-month').addEventListener('keyup', function() {
+    document.querySelector('.exp-month').innerHTML = this.value;
+});
+
+//append expiration year input to virtual card.........................................
+document.getElementById('exp-year').addEventListener('keyup', function() {
+    document.querySelector('.exp-year').innerHTML = this.value;
+});
+
